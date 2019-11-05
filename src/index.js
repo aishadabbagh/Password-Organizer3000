218ca644ee5515f225ae34725c8b4538f862fa17
aishadabbagh <aisha.dabbagh@gmail.com>
2019-11-05 14:50:16 +0300

    clean version of fucked up1

--- src/index.js
+++ src/index.js
@@ -3,10 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-let userEmails = [
-    "Aisha.dabbagh@gmail.com",
-    "Aisha.Dabbagh@alumni.qimam.com",
-    "A.dabbagh@hotmail.com"
+let userEmails = [{
+    ID: 1,
+    Email : "Aisha.dabbagh@gmail.com",
+    "Website": [
+    {"Facebook": "1234"},
+    {"Twitter": "0000"},
+    {"Google": "4321"}] 
+},
+{   ID: 2,
+    Email : "a.dabbagh@hotmail.com",
+    "Website": [
+    {"Yahoo" : "kkkk"},
+    {"Youtube": "1234"}]
+},
+{   ID: 3,
+    Email : "a.d@yahoo.com",
+    "Website": [
+    {"Yahoo" : "kkkk"},
+    {"Youtube": "4444"} ] 
+}
 ];
 
 //Sending user_emails to app//
