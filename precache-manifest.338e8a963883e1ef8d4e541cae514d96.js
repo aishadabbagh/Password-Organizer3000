0d6915e1196bd6d977d8f0dd7ccf5d5423b04843
aishadabbagh <aisha.dabbagh@gmail.com>
2019-11-06 16:15:28 +0300

    Updates

---
+++ precache-manifest.338e8a963883e1ef8d4e541cae514d96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb9472b358f999233129c2f9968b9ee",
+    "url": "/Password-Organizer3000/index.html"
+  },
+  {
+    "revision": "f5a560e011adc94dc5f6",
+    "url": "/Password-Organizer3000/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d6c2392493395352e4df",
+    "url": "/Password-Organizer3000/static/js/2.fb9dc278.chunk.js"
+  },
+  {
+    "revision": "f5a560e011adc94dc5f6",
+    "url": "/Password-Organizer3000/static/js/main.72e45e0b.chunk.js"
+  },
+  {
+    "revision": "6994f5f5f5be628a3550",
+    "url": "/Password-Organizer3000/static/js/runtime-main.b5b34b25.js"
+  }
+]);
